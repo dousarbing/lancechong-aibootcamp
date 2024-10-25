becef57bbb238f84eceee92397a60db015f13f55
dousarbing <dousarbing@gmail.com>
2024-10-25 16:04:57 +0800

    Create methodology.py

---
+++ pages/methodology.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Methodology")
+
+c1 = st.container(border=True)
+c1.header("USE CASE 1")
+c1.subheader("Chatbot for Resale Flat Buying Procedures")
+c1.image("chatbot.jpg")
+
+c2 = st.container(border=True)
+c2.header("USE CASE 2")
+c2.subheader("Resale Flat Price for HDB")
+c2.image("resale_price.jpg")
